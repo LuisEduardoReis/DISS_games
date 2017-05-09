be053f9a324ec6e2e699e64c1739558fc9f046b1
KR-Eddie <luiseddie.kr@gmail.com>
2017-05-09 18:12:06 +0100

    Created snake tutorial

---
+++ tutoriais/snake.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<body>
+
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="lib/default.min.css">
+<script src="lib/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+
+<h2> Snake </h2>
+
+<div id="demo">
+	<?php include('../jogos/snake.html'); ?>
+</div>
+
+<a href="./">Índice</a>
